724b036355ecb42c560c5334bbb01e59460cce39
Admin <reteamboss@gmail.com>
2024-11-06 12:04:02 +0300

    2

--- app/src/main/java/com/example/firebasehw/MainActivity.kt
+++ app/src/main/java/com/example/firebasehw/MainActivity.kt
@@ -9,17 +9,12 @@ class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
-
     }
-
 
     override fun onDestroy() {
         super.onDestroy()
